1eb0d0f0589ccc9ffd052b0dbbb1ab3f04a8fc7c
Bartosz <bg@bartoszgajda.com>
2018-11-02 16:22:01 +0000

    Prints all over.

--- basic.py
+++ basic.py
@@ -3,9 +3,21 @@
 Multi line Comment
 '''
 
+# Prints
 print("Hello World")
 print('Hello World Twice')
-
 print('''Text'and more text''')
 
+# Print substrings
 print("Hello"[0])
+print("Hello"[0:3])
+
+# Print numbers
+print(3)
+print(1, 2, 3, "Numbers")
+
+# Printing in many lines
+print("Line1\nLine2\nLine3")
+
+# Escaping special chars
+print(r"C://Projects")
