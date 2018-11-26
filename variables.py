dd8fae84c4ed5f1f4e284449824d079e7c29dc85
Bartosz <bg@bartoszgajda.com>
2018-11-26 19:33:52 +0000

    Couple basic variables.

--- variables.py
+++ variables.py
@@ -4,3 +4,20 @@ greeting = "Hello Everyone"
 print(greeting)
 
 # Data types
+myStr = "String"
+myInt = 25
+myFloat = 1.3
+
+myList = [1, 2, 3, "First"]
+myDictionary = {"a": 1, "b": 2, "c": 3}
+
+print(type(myStr), myStr)
+print(type(myInt), myInt)
+print(type(myFloat), myFloat)
+print(type(myList), myList)
+print(type(myDictionary), myDictionary)
+
+print(myList[3])
+print(myDictionary["a"])
+
+print(myStr, "World")
