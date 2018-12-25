ac72bc4c80ebe71e14d5d291a16a2aa2681665bc
Bartosz <bg@bartoszgajda.com>
2018-12-25 22:04:46 +0000

    Tuples.

--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,19 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
 
+# Create tuple
+fruits = ("Apples", "Oranges", "Grapes")
+# fruits2 = tuple(("Apples", "Oranges", "Grapes"))
+
+# Single value tuple (needs trailing comma)
+fruits2 = ("Apples",)
+
+# Can't change a value
+# fruits[0] = "Pears"
+
+# Delete tuple
+del fruits2
+
+# Get length
+len(fruits)
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
